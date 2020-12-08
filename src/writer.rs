5cb1e9246067c6e1d3437baa62ea35992dd95fa9
Little Bird <31522351+hummingly@users.noreply.github.com>
2020-12-08 23:19:19 +0100

    Add constants

--- src/writer.rs
+++ src/writer.rs
@@ -2,12 +2,22 @@
 use crate::contentline::{ContentLine, PropertyWrite, Writer};
 use std::io::{Error, Write};
 
+const VCALENDAR: &str = "VCALENDAR";
+const VEVENT: &str = "VEVENT";
+const VTODO: &str = "VTODO";
+const VJOURNAL: &str = "VJOURNAL";
+const VFREEBUSY: &str = "VFREEBUSY";
+const VALARM: &str = "VALARM";
+const VTIMEZONE: &str = "VTIMEZONE";
+const STANDARD: &str = "STANDARD";
+const DAYLIGHT: &str = "DAYLIGHT";
+
 pub struct CalendarWriter<W: Write>(Writer<W>);
 
 impl<W: Write> CalendarWriter<W> {
     pub fn new(inner: W, version: String, product_id: String) -> Result<CalendarWriter<W>, Error> {
         let mut writer = Writer::new(inner);
-        writer.write_begin_unchecked("VCALENDAR")?;
+        writer.write_begin_unchecked(VCALENDAR)?;
         write!(writer, "VERSION:{}", version)?;
         writer.end_line()?;
         write!(writer, "PRODID:{}", product_id)?;
@@ -37,44 +47,44 @@ impl<W: Write> CalendarWriter<W> {
     where
         F: FnOnce(&mut EventWriter<'_, W>) -> Result<(), Error>
     {
-        self.0.write_begin_unchecked("VEVENT")?;
+        self.0.write_begin_unchecked(VEVENT)?;
         let mut writer = EventWriter::new(&mut self.0, uid, dt_stamp)?;
         body(&mut writer)?;
-        self.0.write_end_unchecked("VEVENT")
+        self.0.write_end_unchecked(VEVENT)
     }
 
     pub fn write_todo<F>(&mut self, uid: &str, dt_stamp: &str, body: F) -> Result<(), Error>
     where
         F: FnOnce(&mut TodoWriter<'_, W>) -> Result<(), Error>
     {
-        self.0.write_begin_unchecked("VTODO")?;
+        self.0.write_begin_unchecked(VTODO)?;
         let mut writer = TodoWriter::new(&mut self.0, uid, dt_stamp)?;
         body(&mut writer)?;
-        self.0.write_end_unchecked("VTODO")
+        self.0.write_end_unchecked(VTODO)
     }
 
     pub fn write_journal<F>(&mut self, uid: &str, dt_stamp: &str, body: F) -> Result<(), Error>
     where
         F: FnOnce(&mut JournalWriter<'_, W>) -> Result<(), Error>
     {
-        self.0.write_begin_unchecked("VJOURNAL")?;
+        self.0.write_begin_unchecked(VJOURNAL)?;
         let mut writer = JournalWriter::new(&mut self.0, uid, dt_stamp)?;
         body(&mut writer)?;
-        self.0.write_end_unchecked("VJOURNAL")
+        self.0.write_end_unchecked(VJOURNAL)
     }
 
     pub fn write_free_busy<F>(&mut self, uid: &str, dt_stamp: &str, body: F) -> Result<(), Error>
     where
         F: FnOnce(&mut FreeBusyWriter<'_, W>) -> Result<(), Error>
     {
-        self.0.write_begin_unchecked("VFREEBUSY")?;
+        self.0.write_begin_unchecked(VFREEBUSY)?;
         let mut writer = FreeBusyWriter::new(&mut self.0, uid, dt_stamp)?;
         body(&mut writer)?;
-        self.0.write_end_unchecked("VFREEBUSY")
+        self.0.write_end_unchecked(VFREEBUSY)
     }
 
     pub fn close(mut self) -> Result<W, Error> {
-        self.0.write_end_unchecked("VCALENDAR")?;
+        self.0.write_end_unchecked(VCALENDAR)?;
         self.0.into_inner()
     }
 }
@@ -104,10 +114,10 @@ impl<W: Write> EventWriter<'_, W> {
     where
         F: for<'f> FnOnce(&mut AlarmWriter<'f, W>) -> Result<(), Error>
     {
-        self.0.write_begin_unchecked("VALARM")?;
+        self.0.write_begin_unchecked(VALARM)?;
         let mut alarm = AlarmWriter::new(self.0)?;
         write_fn(&mut alarm)?;
-        self.0.write_end_unchecked("VALARM")
+        self.0.write_end_unchecked(VALARM)
     }
 }
 
@@ -136,10 +146,10 @@ impl<W: Write> TodoWriter<'_, W> {
     where
         F: for<'f> FnOnce(&mut AlarmWriter<'f, W>) -> Result<(), Error>
     {
-        self.0.write_begin_unchecked("VALARM")?;
+        self.0.write_begin_unchecked(VALARM)?;
         let mut alarm = AlarmWriter::new(self.0)?;
         write_fn(&mut alarm)?;
-        self.0.write_end_unchecked("VALARM")
+        self.0.write_end_unchecked(VALARM)
     }
 }
 
